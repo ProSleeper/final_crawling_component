1f686ec0e6897f90547a26c53d69e66063bbbb38
kyw <ingngnin@gmail.com>
2022-08-22 00:01:13 +0900

    야놀자 메인 크롤링 예제

---
+++ createdData.js
@@ -0,0 +1,17 @@
+/* NodeJS를 설치 후 실행해야 실행된다. */
+
+//드림코딩 미니미 쇼핑몰 만들기에서 사용할 json 데이터 만들기
+//이 함수의 내용이 약간 길지만 크게 3개로 나눠진다.
+//1. 데이터부분 2. 랜덤데이터 추출해서 저장 3. 파일로 출력
+function initDraw() {
+  var fs = require("fs");
+  let data = {};
+  data.items = [];
+
+  fs.appendFile("data.json", JSON.stringify(data), function (err) {
+    if (err) throw err;
+    console.log("complete");
+  });
+}
+
+initDraw();
