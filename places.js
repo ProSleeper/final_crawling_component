13881185af4d39b45ca4d02e03b3f3021485b5ea
kyw <ingngnin@gmail.com>
2022-09-08 01:29:56 +0900

    test(places.js): 이미지 다운로드 구현 테스트 중

--- places.js
+++ places.js
@@ -1,100 +1,89 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs");
+const path = require("path");
+const { ServerResponse } = require("http");
 
-// cheerio를 가져온다.
-const cheerio = require("cheerio");
-const MAIN_URL = "https://place-site.yanolja.com/places/3006283";
+const DEFAULT_URL = "https://place-site.yanolja.com/places/3006283";
 /** 
- 
+
 https://place-site.yanolja.com/places/25986
 https://place-site.yanolja.com/places/3006283
 https://place-site.yanolja.com/places/1007912
 https://place-site.yanolja.com/places/1000099554
 
 뒤에 숫자만 바꾸면 된다. 근데 숫자의 기준을 모르겠다 뭐가 지역인지 아니면 숙소의 종류인지
-
 */
 
-(async () => {
-  // 브라우저를 실행한다.
-  // 옵션으로 headless모드를 끌 수 있다.
-  // const browser = await puppeteer.launch();
-  const browser = await puppeteer.launch({
-    headless: false,
-  });
-  // 새로운 창을 연다.
-
+async function startCrawl() {
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.setViewport({
-    width: 1920,
-    height: 1080,
-  });
+  await page.setViewport({ width: 1920, height: 1080 });
+  await page.goto(DEFAULT_URL);
 
-  // 창의 크기를 설정한다.
-  const $ = await loadCheerio(page, MAIN_URL);
-  const shortCuts_addr = main_shortCut(page);
-  //console.log(shortCuts_addr);
-  // const d1_Recommended_Motel = await d1_Region_Select(page, shortCuts_addr[0]);
+  //숙소 크롤링 시작
+  accoCrawl(page);
+}
 
-  // console.log("이거지?" + d1_Recommended_Motel);
-  // 브라우저를 종료한다.
-  //browser.close();
-})();
+//숙소 정보 크롤링(일단 사진부터)
+async function accoCrawl(page) {
+  await readyDownload(page);
+  await startDownloadPicture(page);
 
-//#BOTTOM_SHEET > div.css-qu3ao > div > div.right.css-oq3qu5
+  // const fileName = image.split("/").pop();
+  // const filePath = path.resolve("c:\\", fileName);
+  // const writeStream = fs.createWriteStream(filePath);
+  // writeStream.write(file);
+}
 
-/**
-  메인화면의 바로가기 버튼들의 주소 반환
- */
-async function main_shortCut(page) {
+async function readyDownload(page) {
   const templa = await page.waitForSelector("#BOTTOM_SHEET > div.css-qu3ao > div > div.right.css-oq3qu5"); //해당 요소가 로딩될때까지 기다려주는 코드
-  const btn = await page.$("#__next > div > div > main > article > div:nth-child(1) > section > div.carousel-root > div > div.css-ln49wb");
-
-
-  await templa.click();
-  await templa.click();
-  await templa.click();
-  console.log(btn)
- 
-  await btn.click();
-  
-  // const btn = page.waitForSelector("#__next > div > div > main > article > div:nth-child(1) > section > div.carousel-root > div > div.css-ln49wb:before");
-  // console.log(btn.length)
-  //await btn.click();
-  //console.log(btn.click)
-  // btn.click();
-  // btn.click();
-  // await btn.click();
-  // btn.click();
-  // btn.click();
-  // await btn.click();
-  // btn.click();
-  // btn.click();
-  //console.log(typeof btn);
-  //await btn.click();
- 
+  // const btn = await page.$("#__next > div > div > main > article > div:nth-child(1) > section > div.carousel-root > div > div.css-ln49wb");
+  try {
+    //원래는 3번만 클릭하면 되는데 이렇게 한 이유는
+    //puppeteer에서 지원하는데 내가 구현을 못하는 건지 모르겠는데
+    //어떤 event(보통 클릭)으로 애니메이션이나 트랜지션이 발생했다면 그걸 끝나고 다음 클릭을 해야하는데
+    //정확히 그렇게 구현하는 방법을 모르겠다.
+    //await는 결국 비동기를 동기적으로 만들어주긴 하지만 그게 크롤링할때 애니메이션까지 보장해주지는 못한다.
+    await templa.click();
+    await templa.click();
+    await templa.click();
+    await templa.click();
+    await templa.click();
+  } catch (error) {
+    //console.error(error);
+  }
 }
 
-/**
-  모텔 페이지에서 지역선택 부분이 spa로 되어 있어서 그 부분을 눌러서 하위 메뉴가 나오도록 해주는 함수
-  나머지 페이지들도 class이름이 동일해서 url만 바꿔주면 모두 동일하게 찾아준다.
-  현재는 테스트 모드로 index증가하면서 한개씩 클릭이 잘 되네 해봤다.
-  내일은 눌렀을 때 모든 메뉴가 나오면 다음 코드 실행하는 것으로 만들어보자
-  wait어쩌구 메서드 쓰면 되는 것 같다.
- */
-async function d1_Region_Select(page, url) {
-  await loadCheerio(page, url);
+async function startDownloadPicture(page) {
+  //const elements = await page.$x("/html/body/div[1]/div/div/main/article/div[1]/section/div[1]/div/div[3]");
+  const temp = await page.waitForSelector("#__next > div > div > main > article > div:nth-child(1) > section > div.carousel-root > div > div.css-ln49wb");
+  // console.log(temp);
 
-  //요 아래에서 에러 나네
-
-  
- 
+  for (let index = 0; index < 5; index++) {
+    await page.waitForTimeout(1000);
+    await temp.click();
+    await savePicture(page, index);
+  }
 }
 
-async function loadCheerio(page, url) {
-  /* 페이지에 접속한다. */
-  await page.goto(url);
-  /* 페이지의 HTML을 가져온다. */
-  const content = await page.content();
-  // $에 cheerio를 로드한다.
-  return cheerio.load(content);
+//뭔가 다운이 되기는 하는데 이건 그냥 된다 느낌으로 행복감만 충족하자
+//다른 구현으로 만들자. 내가 조종할 수 있는 코드로
+async function savePicture(page, index) {
+  await page.$x("//*[@id='" + index + "']/div/span/img");
+  // const src = await target.getProperty("src");
+  // const image = await src.jsonValue();
+  page.on("response", async (response) => {
+    const url = response.url();
+    console.log(url);
+    if (response.request().resourceType() === "image") {
+      response.buffer().then((file) => {
+        const fileName = url.split("/").pop();
+        const filePath = path.resolve("/image/", fileName);
+        const writeStream = fs.createWriteStream(filePath);
+        writeStream.write(file);
+      });
+    }
+  });
 }
+
+startCrawl();
