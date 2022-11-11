d2e6f144137e3a8b88e81e0fd8e4e7ac6b0db72f
kyw <ingngnin@gmail.com>
2022-11-11 13:44:17 +0900

    refactor: 비주얼 스튜디오에서 f5 누르면 해당 파일이 디버그 모드로 실행되도록 설정json 변경

--- test.js
+++ test.js
@@ -1,13 +1,26 @@
+function solution(ingredient) {
+  var answer = 0;
+  const ORIGIN_SIZE = ingredient.length;
+  const HAMBERGER = "1231";
+  let strIngredient = ingredient.join("");
+  let prevStrIngredient = "";
 
 
+  ingredient.reduce((a, b, index) => {
+    const text = (a + "" + b);
+    if (text.indexOf(HAMBERGER) !== -1) {
+      text.replace()
+    }
 
-for (let index = 0; index < 161; index++) {
-  for (let j = 0; j < 3; j++) {
-    const number = Math.floor(Math.random() * 160 + 1)
-    console.log(`(${index + 1}, "image${number}.jpg", "image${number}.jpg")`);
-    
-  }
-  
+  })
+
+
+  const RESULT_SIZE = strIngredient.length;
+
+  answer = (ORIGIN_SIZE - RESULT_SIZE) / 4;
+
+  return answer;
 }
 
-// console.log(`(${index + 1}, "image${index}.jpg", "image${index}.jpg")`);
+solution([2, 1, 1, 2, 3, 1, 2, 3, 1]);
+// solution([1, 3, 2, 1, 2, 1, 3, 1, 2]);
